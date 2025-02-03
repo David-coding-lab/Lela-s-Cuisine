5114d682f7a58bb85383b3ed8c09c75e1534636f
Care Tech Hub <caretechhub@gmail.com>
2025-02-03 07:00:24 +0100

    added the book reservation  page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,10 +1,92 @@
-import { Box } from '@chakra-ui/react'
+import { Badge, Box, Button, Flex, HStack, Image, Input, InputGroup, Select, Text } from '@chakra-ui/react'
+import reservationImg from '../assets/reservationImg.png'
 
 function Booking() {
     return (
-        <Box>
-            Booking
-        </Box>
+        <Flex
+            justify='space-evenly'
+            align='center'
+            pt='50px'
+        >
+            <Flex
+                flexDir='column'
+                align='center'
+                justify='center'
+                gap='10px'
+            >
+                <Text
+                    fontFamily='Leckerli One'
+                    fontSize='x-large'
+                    color='#554A4A'
+                >Book a Reservation</Text>
+                <Image
+                    src={reservationImg}
+                    width={{base: '150px', sm: '200px', md: '250px', lg: '450px'}}
+                />
+            </Flex>
+
+            <Flex
+                w='400px'
+                height='450px'
+                bgColor='rgba(199, 199, 199, 0.26)'
+                padding='40px'
+                flexDir='column'
+                gap='40px'
+                color='#554A4A'
+                fontFamily='Mogra'
+                borderRadius='10px'
+                justify='center'
+
+            >
+                <InputGroup display='flex' flexDir='column'>
+                    <Text>Name</Text>
+                    <Input
+                        border='none'
+                        borderBottom='1px solid #5E4949'
+                        bgColor='transparent'
+                        borderRadius='0'
+                        width='300px'
+                    />
+                </InputGroup>
+
+                <InputGroup display='flex' flexDir='column'>
+                    <Text>Phone</Text>
+                    <Input
+                        border='none'
+                        borderBottom='1px solid #5E4949'
+                        bgColor='transparent'
+                        borderRadius='0'
+                        width='300px'
+                    />
+                </InputGroup>
+
+                <InputGroup display='flex' flexDir='column'>
+                    <Text>Date</Text>
+                    <Input
+                        type='date'
+                        border='none'
+                        borderBottom='1px solid #5E4949'
+                        bgColor='transparent'
+                        borderRadius='0'
+                        width='300px'
+                    />
+                </InputGroup>
+
+                <Button
+                    w='150px'
+                    h='40px'
+                    bgColor='#5E4949'
+                    color='white'
+                    display='flex'
+                    alignSelf='flex-end'
+                    _hover={{
+                        bgColor: 'transparent',
+                        border: '1px solid #5E4949',
+                        color: '#5E4949'
+                    }}
+                >Book</Button>
+            </Flex>
+        </Flex>
     )
 }
 
